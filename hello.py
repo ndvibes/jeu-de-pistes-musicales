31b7554c533699eb29993fb1ba9dc4d279560f5e
Nahom Daniel <nahom.daniel@gmail.com>
2017-01-31 01:41:02 +0100

    Working out how Flask static files and Jinja 2 templating works.

--- hello.py
+++ hello.py
@@ -1,16 +1,24 @@
 from flask import Flask
 from flask import render_template
+import json
+import os
+
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return render_template('hello.html')
+    message = "Hello !"
+    return render_template(
+        'hello.html',
+        message=message)
 
 
 @app.route("/game-won")
 def game_won():
-    return render_template('game_won.html')
+    message = "You won !"
+    return render_template('game_won.html', message=message)
 
 
 if __name__ == "__main__":
